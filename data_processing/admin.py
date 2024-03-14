d5ed193d188672dc7bfde81c36494a753b31a29e
Dmitriy Rodionov <dmitr.rod@gmail.com>
2024-03-14 15:42:58 +0300

    Разворачивание приложения

--- data_processing/admin.py
+++ data_processing/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from data_processing.models import WOTalon
+
+
+class DataProcessingAdmin(admin.ModelAdmin):
+    list_display = ('talon_id', 'last_updated')
+
+
+admin.site.register(WOTalon, DataProcessingAdmin)
